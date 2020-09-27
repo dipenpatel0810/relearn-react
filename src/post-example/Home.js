df5fdadfa5b31b449589b8c44ed84927a38ec7fa
Dipen Patel <patel.dip@husky.neu.edu>
2020-09-27 19:00:50 -0400

    [MOD] Modified list to have better styling

--- src/post-example/Home.js
+++ src/post-example/Home.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 const PostFeed = require('../services/PostFeed');
 
+const useStyles = makeStyles(() => ({
+    root: {
+      width: '100%',
+      maxWidth: 'auto',
+      backgroundColor: 'lightgrey',
+    },
+  }));
+
 export default function Home() {
+    const classes = useStyles();
     const [posts, setPosts] = React.useState([]);
 
     React.useEffect(() => {
@@ -9,19 +24,22 @@ export default function Home() {
     });
     return (
         <React.Fragment>
-            Post Data
-            <ul>
-                {
-                    posts.map(post => {
-                        return (<React.Fragment>
-                            <li>{post.userId}</li>
-                            <li>{post.title}</li>
-                            <br />
-                        </React.Fragment>)
-                    })
-                }
-            </ul>
-
+            <Container>
+                Post Data
+                <List className={classes.root}>
+                    {
+                        posts.map(post => {
+                            return (
+                                <React.Fragment>
+                                    <ListItem>
+                                        <ListItemText primary={post.title} secondary={post.body}/>
+                                    </ListItem>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </List>
+            </Container>
         </React.Fragment>
     )
 }
